be2027474259fb6f49d3d519627008b126eeab3d
youzhuaizaifei <ericbox711@gmail.com>
2025-08-14 14:01:47 +0800

    fix router

--- src/router.js
+++ src/router.js
@@ -4,6 +4,9 @@ import TeacherLogin from './components/TeacherLogin.vue';
 import TeacherDashboard from './components/TeacherDashboard.vue';
 import TeacherProfile from './components/TeacherProfile.vue';
 import TestPage from './components/testPage.vue';
+import TeacherTrialApplication from './components/TeacherTrialApplication.vue';
+import TeacherAttendance from './components/TeacherAttendance.vue';
+  
 
 const routes = [
   {
@@ -25,6 +28,14 @@ const routes = [
   {
     path: '/testPage',
     component: TestPage
+  },
+  {
+    path: '/teachertrialapplication',
+    component: TeacherTrialApplication
+  },
+  {
+    path: '/teacherattendance',
+    component: TeacherAttendance
   }
 ];
 
